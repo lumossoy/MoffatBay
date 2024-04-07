6d01ae70ea5a13e880fe6b1e4df20264bbdd81c5
kaisch006 <134887301+kaisch006@users.noreply.github.com>
2024-04-07 17:22:51 -0400

    Update MoffatBayProject.sql
    - Fixed additional syntax errors
    - reorganized to avoid missing data fields when inserting information

--- MoffatBayProject.sql
+++ MoffatBayProject.sql
@@ -1,5 +1,6 @@
--- Green Team 
+-- Green Team (Estiven Hernandez, Chris Kaiser, Emely Pajarito, Juan Taylor)
 -- Moffat Bay MySQL Table
+-- This SQL creates our Database and Initial Entries
 
 -- Database creation
 DROP DATABASE IF EXISTS MoffatBay;
@@ -17,12 +18,6 @@ CREATE TABLE IF NOT EXISTS Users (
     PRIMARY KEY (UserID)
 ) ENGINE=InnoDB;
 
-- Initial Users
-INSERT INTO Users (Email, Password, FirstName, LastName, TelephoneNumber) VALUES
-('johnrega21@hotmail.com', '123Password!', 'John', 'Rega', 678942332),
-('alexharper@gmail.com', 'NatureRocks!', 'Alex', 'Harper', 404897123),
-('steph27@ymail.com', 'CorporateBaddie1', 'Stephanie', 'Culler', 710987678);
-
 -- Reservations Table
 CREATE TABLE IF NOT EXISTS Reservations (
     ReservationID INT AUTO_INCREMENT NOT NULL,
@@ -32,12 +27,6 @@ CREATE TABLE IF NOT EXISTS Reservations (
     ConfirmationNum INT NOT NULL,
     PRIMARY KEY (ReservationID)
 ) ENGINE=InnoDB;
-
-- Initial Reservations
-INSERT INTO Reservations (CheckInDate, CheckOutDate, TotalGuests, ConfirmationNum) VALUES
-('2024-04-01', '2024-04-07', 4, 105764),
-('2024-07-18', '2024-07-25', 1, 105768),
-('2024-10-24', '2024-11-24', 2, 105769);
 
 -- Rooms Table
 CREATE TABLE IF NOT EXISTS Rooms (
@@ -49,12 +38,6 @@ CREATE TABLE IF NOT EXISTS Rooms (
     FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
 ) ENGINE=InnoDB;
 
--- Initial Rooms
-INSERT INTO Rooms (ReservationID, RoomType, RoomPrice) VALUES
-(1, 'double queen', 115.00),
-(2, 'king', 135.00);
-(3, 'suite', 300.00);
-
 -- Amenities Table
 CREATE TABLE IF NOT EXISTS Amenities (
     AmenityID INT AUTO_INCREMENT NOT NULL,
@@ -63,13 +46,6 @@ CREATE TABLE IF NOT EXISTS Amenities (
     PRIMARY KEY (AmenityID),
     FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID)
 ) ENGINE=InnoDB;
-
--- Initial Amenities
-INSERT INTO Amenities (RoomID, AmenityName) VALUES
-(1, 'High Speed WiFi'),    
-(2, 'Birthday Celebration Pckg'),
-(3, 'Anniversary Celebration Pckg')    
-(3, 'Hiking Package');
 
 -- Attractions Table
 CREATE TABLE IF NOT EXISTS Attractions (
@@ -80,13 +56,6 @@ CREATE TABLE IF NOT EXISTS Attractions (
     FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
 ) ENGINE=InnoDB;
 
--- Initial Attractions 
-INSERT INTO Attractions (ReservationID, Activity) VALUES
-(3, 'Hiking'),
-(3, 'Kayaking'),
-(2, 'Scuba Diving'),
-(1, 'Whale Watching');
-
 -- User_Reservations Table
 CREATE TABLE IF NOT EXISTS User_Reservations (
     UserID INT NOT NULL,
@@ -96,8 +65,40 @@ CREATE TABLE IF NOT EXISTS User_Reservations (
     FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
 ) ENGINE=InnoDB;
 
--- Initial User Reservations
+-- Add Initial Users
+INSERT INTO Users (Email, Password, FirstName, LastName, TelephoneNumber) VALUES
+('johnrega21@hotmail.com', '123Password!', 'John', 'Rega', '706-894-2332'),
+('alexharper@gmail.com', 'NatureRocks!', 'Alex', 'Harper', '654-489-7123'),
+('steph27@ymail.com', 'CorporateBaddie1', 'Stephanie', 'Culler', '249-987-6789');
+
+-- Add Initial Reservations
+INSERT INTO Reservations (CheckInDate, CheckOutDate, TotalGuests, ConfirmationNum) VALUES
+('2024-04-01', '2024-04-07', 4, 105764),
+('2024-07-18', '2024-07-25', 1, 105768),
+('2024-10-24', '2024-11-24', 2, 105769);
+
+-- Add Initial Rooms
+INSERT INTO Rooms (ReservationID, RoomType, RoomPrice) VALUES
+(1, 'double queen', 115.00),
+(2, 'king', 135.00),
+(3, 'suite', 300.00);
+
+-- Add Initial Amenities
+INSERT INTO Amenities (RoomID, AmenityName) VALUES
+(1, 'High Speed WiFi'),    
+(2, 'Birthday Pckg'),
+(3, 'Anniversary Pckg'),    
+(3, 'Hiking Pckg');
+
+-- Add Initial Attractions 
+INSERT INTO Attractions (ReservationID, Activity) VALUES
+(3, 'Hiking'),
+(3, 'Kayaking'),
+(2, 'Scuba Diving'),
+(1, 'Whale Watching');
+
+-- Add Initial User Reservations
 INSERT INTO User_Reservations (UserID, ReservationID) VALUES
 (1, 1),
 (2, 2),
-(3, 3),
+(3, 3);
