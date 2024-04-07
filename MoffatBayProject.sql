247f9fd05f3074aa73575a159d03560f6e44141c
kaisch006 <134887301+kaisch006@users.noreply.github.com>
2024-04-07 16:49:55 -0400

    Update MoffatBayProject.sql
    - Added NOT NULL values per Emely's suggestion
    - Removed default increment changes to keep it simple (this is supposed to be a new database afterall)
    - Fixed a few syntax errors
    - Changed Telephone Number to VARCHAR(20) to accommodate a variety of telephone number options

--- MoffatBayProject.sql
+++ MoffatBayProject.sql
@@ -1,67 +1,70 @@
 -- Green Team 
 -- Moffat Bay MySQL Table
--- Quick team notes: 
--- Chris: Tweaked to match a more finalized ERD. Changed some of the auto_increments as I don't think they were necessary (but we can change them back before submitting)
--- Also cleaned up some of the comments for better readability now that the changes addressed in the comments were implimented and reviewed 
+
+-- Database creation
+DROP DATABASE IF EXISTS MoffatBay;
+CREATE DATABASE MoffatBay;
+USE MoffatBay;
 
 -- Users Table
 CREATE TABLE IF NOT EXISTS Users (
-    UserID INT AUTO_INCREMENT,
-    FirstName VARCHAR(25),
-    LastName VARCHAR(25),
-    Email VARCHAR(40),
-    Password VARCHAR(25),
-    TelephoneNumber INT,
+    UserID INT AUTO_INCREMENT NOT NULL,
+    FirstName VARCHAR(25) NOT NULL,
+    LastName VARCHAR(25) NOT NULL,
+    Email VARCHAR(40) NOT NULL,
+    Password VARCHAR(25) NOT NULL,
+    TelephoneNumber VARCHAR(20),
     PRIMARY KEY (UserID)
 ) ENGINE=InnoDB;
 
--- Set AUTO_INCREMENT to start at 1000 for Users but we can always adjust this to what the team sees fit
-ALTER TABLE Users AUTO_INCREMENT = 1000;
-
+- Initial Users
 INSERT INTO Users (Email, Password, FirstName, LastName, TelephoneNumber) VALUES
 ('johnrega21@hotmail.com', '123Password!', 'John', 'Rega', 678942332),
 ('alexharper@gmail.com', 'NatureRocks!', 'Alex', 'Harper', 404897123),
 ('steph27@ymail.com', 'CorporateBaddie1', 'Stephanie', 'Culler', 710987678);
 
--- Rooms
-CREATE TABLE IF NOT EXISTS Rooms (
-    RoomID INT AUTO_INCREMENT,
-    ReservationID INT,
-    RoomType ENUM('double queen', 'king', 'suite') NOT NULL,
-    RoomPrice DECIMAL(8, 2),
-    PRIMARY KEY (RoomID)
-    FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
-) ENGINE=InnoDB;
-
-INSERT INTO Rooms (Reservation ID, RoomType, RoomPrice) VALUES
-(1, 'double queen', 115.00),
-(2, 'king', 135.00);
-(3, 'suite', 300.00);
-
 -- Reservations Table
 CREATE TABLE IF NOT EXISTS Reservations (
-    ReservationID INT AUTO_INCREMENT,
-    CheckInDate DATE,
-    CheckOutDate DATE,
-    TotalGuests INT,
-    ConfirmationNum INT,
-    PRIMARY KEY (ReservationID),
+    ReservationID INT AUTO_INCREMENT NOT NULL,
+    CheckInDate DATE NOT NULL,
+    CheckOutDate DATE NOT NULL,
+    TotalGuests INT NOT NULL,
+    ConfirmationNum INT NOT NULL,
+    PRIMARY KEY (ReservationID)
 ) ENGINE=InnoDB;
 
+- Initial Reservations
 INSERT INTO Reservations (CheckInDate, CheckOutDate, TotalGuests, ConfirmationNum) VALUES
 ('2024-04-01', '2024-04-07', 4, 105764),
 ('2024-07-18', '2024-07-25', 1, 105768),
 ('2024-10-24', '2024-11-24', 2, 105769);
 
+-- Rooms Table
+CREATE TABLE IF NOT EXISTS Rooms (
+    RoomID INT AUTO_INCREMENT NOT NULL,
+    ReservationID INT NOT NULL,
+    RoomType ENUM('double queen', 'king', 'suite') NOT NULL,
+    RoomPrice DECIMAL(8, 2) NOT NULL,
+    PRIMARY KEY (RoomID),
+    FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
+) ENGINE=InnoDB;
+
+-- Initial Rooms
+INSERT INTO Rooms (ReservationID, RoomType, RoomPrice) VALUES
+(1, 'double queen', 115.00),
+(2, 'king', 135.00);
+(3, 'suite', 300.00);
+
 -- Amenities Table
 CREATE TABLE IF NOT EXISTS Amenities (
-    AmenityID INT AUTO_INCREMENT,
-    RoomID INT,
-    AmenityName VARCHAR(25),
+    AmenityID INT AUTO_INCREMENT NOT NULL,
+    RoomID INT NOT NULL,
+    AmenityName VARCHAR(25) NOT NULL,
     PRIMARY KEY (AmenityID),
     FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID)
 ) ENGINE=InnoDB;
 
+-- Initial Amenities
 INSERT INTO Amenities (RoomID, AmenityName) VALUES
 (1, 'High Speed WiFi'),    
 (2, 'Birthday Celebration Pckg'),
@@ -70,13 +73,14 @@ INSERT INTO Amenities (RoomID, AmenityName) VALUES
 
 -- Attractions Table
 CREATE TABLE IF NOT EXISTS Attractions (
-    AttractionsID INT AUTO_INCREMENT,
-    ReservationID INT,
-    Activity VARCHAR(30),
+    AttractionsID INT AUTO_INCREMENT NOT NULL,
+    ReservationID INT NOT NULL,
+    Activity VARCHAR(30) NOT NULL,
     PRIMARY KEY (AttractionsID),
     FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
 ) ENGINE=InnoDB;
 
+-- Initial Attractions 
 INSERT INTO Attractions (ReservationID, Activity) VALUES
 (3, 'Hiking'),
 (3, 'Kayaking'),
@@ -85,13 +89,14 @@ INSERT INTO Attractions (ReservationID, Activity) VALUES
 
 -- User_Reservations Table
 CREATE TABLE IF NOT EXISTS User_Reservations (
-    UserID INT,
-    ReservationID INT,
+    UserID INT NOT NULL,
+    ReservationID INT NOT NULL,
     PRIMARY KEY (UserID, ReservationID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID),
     FOREIGN KEY (ReservationID) REFERENCES Reservations(ReservationID)
 ) ENGINE=InnoDB;
 
+-- Initial User Reservations
 INSERT INTO User_Reservations (UserID, ReservationID) VALUES
 (1, 1),
 (2, 2),
