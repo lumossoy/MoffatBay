8d375e3c3f9fa1ad9b22012a9c87b4ec7bd2355f
Swo0n <111103076+Swo0n@users.noreply.github.com>
2024-05-17 16:26:00 -0400

    Update RegisterDAO.java fixed error of adding user

--- RegisterDAO.java
+++ RegisterDAO.java
@@ -1,8 +1,7 @@
+ // Green Team: Juan Taylor, Christopher Kaiser, Emely Pajarito, Estiven Hernandez
+ // Purpose: Data Access Object to add a new user into database
 package myBean;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
 import java.sql.*;
 
 public class RegisterDAO {
@@ -15,36 +14,49 @@ public class RegisterDAO {
 	public RegisterDAO() {
 
 	}
-
 	
-	//the add User method will insert new users into the database.
-	public String addUser(User newUser) throws SQLException,ClassNotFoundException {
-		String jdbcURL = "jdbc:mysql://localhost:3306/MoffatBayTest";
-		String dbUser = "root";
-		String dbPassword = "theAlphaBlack23@";
+	//the add User method will check for existing e-mail then add the new user
+	public String addUser(User newUser) throws SQLException, ClassNotFoundException {
+	    String jdbcURL = "jdbc:mysql://localhost:3306/MoffatBayTest";
+	    String dbUser = "root";
+	    String dbPassword = "theAlphaBlack23@";
 
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
-		String result = "New Account created! User registered.";
-		try{  
-		String sql = "INSERT INTO users (firstName, lastName, email, password, telephoneNumber) VALUES (?,?,?,?,?)";
-		PreparedStatement statement = connection.prepareStatement(sql);  
-		statement.setString(1,newUser.getFirstName());  
-		statement.setString(2,newUser.getLastName());  
-		statement.setString(3,newUser.getEmail()); 
-		statement.setString(4,newUser.getPassword()); 
-		statement.setString(5,newUser.getTelephoneNumber()); 
-		statement.executeUpdate();  	
-		
-		}catch(Exception e){
-			result = "Unable to create account! User not registered.";
-			System.out.println(newUser.getFirstName());
-			System.out.println(newUser.getLastName());
-			System.out.println(newUser.getEmail());
-			System.out.println(newUser.getPassword());
-			System.out.println(newUser.getTelephoneNumber());
-			e.printStackTrace();
-		}  		      
-		return result;  
-	}  
+	    Class.forName("com.mysql.cj.jdbc.Driver");
+	    Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+	    String result = "New Account created! User registered.";
+
+	    try {
+	        // Check if the email already exists
+	        String checkSql = "SELECT * FROM users WHERE email = ?";
+	        PreparedStatement checkStatement = connection.prepareStatement(checkSql);
+	        checkStatement.setString(1, newUser.getEmail());
+	        ResultSet resultSet = checkStatement.executeQuery();
+
+	        if (resultSet.next()) {
+	            return "An account already exists with this e-mail!";
+	        }
+
+	        // Add new user if new e-mail
+	        String sql = "INSERT INTO users (firstName, lastName, email, password, telephoneNumber) VALUES (?,?,?,?,?)";
+	        PreparedStatement statement = connection.prepareStatement(sql);
+	        statement.setString(1, newUser.getFirstName());
+	        statement.setString(2, newUser.getLastName());
+	        statement.setString(3, newUser.getEmail());
+	        statement.setString(4, newUser.getPassword());
+	        statement.setString(5, newUser.getTelephoneNumber());
+	        statement.executeUpdate();
+
+	    } catch (Exception e) {
+	        result = "Unable to create account! User not registered.";
+	        System.out.println(newUser.getFirstName());
+	        System.out.println(newUser.getLastName());
+	        System.out.println(newUser.getEmail());
+	        System.out.println(newUser.getPassword());
+	        System.out.println(newUser.getTelephoneNumber());
+	        e.printStackTrace();
+	    } finally {
+	        connection.close();
+	    }
+	    return result;
+	} 
 }
