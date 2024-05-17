0abca6e50a67db3b2e25e56ef7ff387d5f527fd7
Swo0n <111103076+Swo0n@users.noreply.github.com>
2024-05-17 16:27:25 -0400

    Update RegisterServlet.java to allow for encryptedPassword

--- RegisterServlet.java
+++ RegisterServlet.java
@@ -1,66 +1,60 @@
+ // Green Team: Juan Taylor, Christopher Kaiser, Emely Pajarito, Estiven Hernandez
+ // Purpose: Servlet to handle user registration requests from register.jsp
 package myBean;
 
 import java.io.*;
-import java.sql.SQLException;
-
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-/**
-Servlet Implementation class RegisterServlet
-maybe change servlet to /register
-also need to add password verfication to ensure password matches and meet requirements//hashing
-ensure users cannot enter undesired elemets or characters into the fields
- */
 @WebServlet("/signup")
 public class RegisterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
- 
+
     public RegisterServlet() {
         super();
     }
-    
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	response.getWriter().append("Serverd at: ").append(request.getContextPath());
-    	
+        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
- 
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String telephoneNumber = request.getParameter("telephoneNumber");
+        String plainPassword = request.getParameter("password");
+        String encryptedPassword = User.encryptPassword(plainPassword);
+        String telephoneNumber = request.getParameter("telephone");
         
-        /*also need to add password verification to ensure password matches and meet requirements//hashing ensure 
-         * users cannot enter undesired elements or characters into the fields
-         *  */
+        System.out.println("Servlet  info:" + firstName);
+        System.out.println("Servlet  info:" + lastName);
+        System.out.println("Servlet  info:" + email);
+        System.out.println("Servlet  info:" + plainPassword);
+        System.out.println("Servlet  info:" + telephoneNumber);
         
-        User newUser = new User(firstName, lastName, email, password, telephoneNumber);
-         
+        
+
+        User newUser = new User(firstName, lastName, email, encryptedPassword, telephoneNumber);
+
         RegisterDAO registerDAO = new RegisterDAO();
-        String result;
-		try {
-			result = registerDAO.addUser(newUser);
-			response.getWriter().println(result);  
-			String destPage = "registration.jsp";
-            
-            if (result == "New Account created! User registered.") {
+        try {
+            String result = registerDAO.addUser(newUser);
+            String destPage = "registration.jsp";
+
+            if ("New Account created! User registered.".equals(result)) {
                 HttpSession session = request.getSession();
                 session.setAttribute("user", newUser);
-                destPage = "home.jsp";
+                destPage = "landingPage.jsp";
             } else {
-                String message = "Error";
-                request.setAttribute("message", message);
+                request.setAttribute("message", result);
             }
-             
+
             RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
             dispatcher.forward(request, response);
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-               
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("Database connection problem.");
+        }
     }
 }
